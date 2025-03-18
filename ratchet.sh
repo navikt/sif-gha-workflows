5d0aa913b0869752c07f05f3018ce547ff4b7b16
Anders Østby <anders1989@gmail.com>
2025-03-18 12:52:51 +0100

    Bumper gha versjoner med ratchet (#220)

--- ratchet.sh
+++ ratchet.sh
@@ -3,5 +3,8 @@
 FILES=$(find .github -type f -name "*.yml")
 
 for FILE in $FILES; do
-    docker run -it --rm -v "${PWD}:${PWD}" -w "${PWD}" ghcr.io/sethvargo/ratchet:0.6.0 pin $FILE
+    docker run -it --rm -v "${PWD}:${PWD}" -w "${PWD}" ghcr.io/sethvargo/ratchet:0.10.2 pin $FILE
+
+    #Med github token
+    #docker run -it --rm -e GITHUB_TOKEN="${GITHUB_TOKEN}" -v "${PWD}:${PWD}" -w "${PWD}" ghcr.io/sethvargo/ratchet:0.10.2 pin $FILE
 done
