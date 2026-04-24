74f4f08ee311465cc562c1f9aea52aa92d151b3d
Qadeer Khan <qadeer.ahmad.khan@nav.no>
2026-04-24 12:31:44 +0200

    Omdøp testmappe til sif-code-scan, oppdater README

--- .github/actions/sif-code-scan/vitest.config.ts
+++ .github/actions/sif-code-scan/vitest.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
-    include: ["../../../tests/sif_code_scan/**/*.test.ts"],
+    include: ["../../../tests/sif-code-scan/**/*.test.ts"],
   },
 });
